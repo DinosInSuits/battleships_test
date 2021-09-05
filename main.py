f7c6ccc9211194cca5a66aede1d366941e48e1e0
DinosInSuits <dinosinsuits@gmail.com>
2021-09-05 00:22:12 -0700

    Added a way to render board and shots fired through coordinate system

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def render(board_width, board_height, shots):
+    header = "+" + "-" * board_width + "+"
+    print(header)
 
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if(x, y) in shots_set:
+                shots_marker = 'X'
+            else:
+                shots_marker = ' '
+            row.append(shots_marker)
+        print('|' + "".join(row) + "|")
 
-# Press the green button in the gutter to run the script.
+    print(header)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    shots = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        player_coordinates = input('Please enter your firing coordinates: ')
+        # TODO: incorporate a means of validating player input.
+        xstr, ystr = player_coordinates.split(',')
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x, y))
+        render(10, 10, shots)
+
